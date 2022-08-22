5221d96c6b4fe372607a73bfd7dbe77bf6ad0c60
Joey Pettit <pettitbass@gmail.com>
2022-08-21 19:13:57 -0500

    created the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,29 @@
 console.log('***** Music Collection *****')
+
+
+//Global Variables
+
+let collection = [];
+
+
+
+//~~~~ Function addToCollection
+
+function addToCollection(albumTitle, albumArtist, albumYear) {
+    let record = {
+        title: albumTitle,
+        artist: albumArtist,
+        yearPublished: albumYear
+    };
+    collection.push(record);
+    return record;
+}
+
+// ~~~~~~ addToCollection Test:
+
+console.log(addToCollection("American Idiot", "Green Day", 2004));
+console.log(addToCollection("A Love Supreme", "John Coltrane", 1964));
+console.log(addToCollection("Innervisions", "Stevie Wonder", 1974));
+console.log(addToCollection("Songs in the Key of Life", "Stevie Wonder", 1976));
+console.log(addToCollection)
+console.log(collection);
